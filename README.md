61405216e969ac591776b775d51e9c4018c26255
Zhaoxian-Wu <wuzhaoxian97@gmail.com>
2026-01-11 18:53:10 -0500

    Change to a new template

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
-# Zhaoxian Wu
+### Acknowledgement
+This website is built on a Jekyll template by Martin Saveski, Mor Naaman, and Quan Xiao, with improvement ideas borrowed from the al‑folio theme.
 
-This is Zhaoxian's website.
+### Updates guidance
+Please modify one of the files in `_data`—unless you plan to change the website’s appearance.
+
+Test changes with:
+```
+jekyll serve --livereload
+```
+
+If a `jekyll serve --livereload` process is already running, please follow these steps:
+```
+lsof -i :4000
+kill -9 <PID>
+
+jekyll serve --livereload
+```
+
+Push to github
+```
+git add .
+git commit -m "Description of changes"
+git push
+```
+
+
+If not working (probabily due to first time or large change), try
+```
+git config http.postBuffer 524288000
+```
+before
+```
+git push
+```
+
+
+## External Libraries
+- Framework: [Jekyll](http://jekyllrb.com/)
+- CSS
+  - [Skeleton](getskeleton.com)
+  - Tabs: [Skeleton Tabs](https://github.com/nathancahill/skeleton-tabs)
+  - Experience: [Timeline](https://codepen.io/NilsWe/pen/FemfK)
+  - Icons: [Font Awesome](http://fontawesome.io/)
+- JS
+  - [Jquery (3.1.1)](https://jquery.com/)
---
+++ libs/custom/my_js.js
@@ -0,0 +1,116 @@
+$(document).ready(function() {
+
+  // Variables
+  var $codeSnippets = $('.code-example-body'),
+      $nav = $('.navbar'),
+      $body = $('body'),
+      $window = $(window),
+      $popoverLink = $('[data-popover]'),
+      navOffsetTop = $nav.offset().top,
+      $document = $(document),
+      entityMap = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': '&quot;',
+        "'": '&#39;',
+        "/": '&#x2F;'
+      }
+
+  function init() {
+    $window.on('scroll', onScroll)
+    $window.on('resize', resize)
+    $popoverLink.on('click', openPopover)
+    $document.on('click', closePopover)
+    $('a[href^="#"]').on('click', smoothScroll)
+    buildSnippets();
+  }
+
+  function smoothScroll(e) {
+    e.preventDefault();
+    $(document).off("scroll");
+    var target = this.hash,
+        menu = target;
+    $target = $(target);
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top-40
+    }, 0, 'swing', function () {
+        window.location.hash = target;
+        $(document).on("scroll", onScroll);
+    });
+  }
+
+  function openPopover(e) {
+    e.preventDefault()
+    closePopover();
+    var popover = $($(this).data('popover'));
+    popover.toggleClass('open')
+    e.stopImmediatePropagation();
+  }
+
+  function closePopover(e) {
+    if($('.popover.open').length > 0) {
+      $('.popover').removeClass('open')
+    }
+  }
+
+  $("#button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#elementtoScrollToID").offset().top
+    }, 2000);
+});
+
+  function resize() {
+    $body.removeClass('has-docked-nav')
+    navOffsetTop = $nav.offset().top
+    onScroll()
+  }
+
+  function onScroll() {
+    if(navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')) {
+      $body.addClass('has-docked-nav')
+    }
+    if(navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')) {
+      $body.removeClass('has-docked-nav')
+    }
+  }
+
+  function escapeHtml(string) {
+    return String(string).replace(/[&<>"'\/]/g, function (s) {
+      return entityMap[s];
+    });
+  }
+
+  function buildSnippets() {
+    $codeSnippets.each(function() {
+      var newContent = escapeHtml($(this).html())
+      $(this).html(newContent)
+    })
+  }
+
+  // Ensure profile image column doesn't exceed text column height
+  function capProfileImageHeight() {
+    var $img = $("img.profile");
+    var $text = $(".main-description");
+    if ($img.length && $text.length) {
+      var h = $text.outerHeight();
+      // Set max-height to text column height
+      $img.css({
+        'max-height': h + 'px',
+        'height': 'auto'
+      });
+    }
+  }
+
+  // Run on load and resize (after fonts/layout settle)
+  $window.on('load', function(){
+    capProfileImageHeight();
+    // slight delay to handle async font/icon loading
+    setTimeout(capProfileImageHeight, 150);
+  });
+  $window.on('resize', capProfileImageHeight);
+
+
+  init();
+
+});
